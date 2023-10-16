691f9ea8a40ba8e19634896e40424d2a8d941406
f-elix <guerinfelix08@gmail.com>
2023-10-16 18:33:11 -0400

    Drop cjs support

--- dist/mjs/constants.d.ts
+++
@@ -1,3 +0,0 @@
-import { Replacer, Screen } from 'types';
-export declare const DEFAULT_SCREEN: Screen;
-export declare const BASE_REPLACER: Replacer;
--- dist/mjs/transform.d.ts
+++
@@ -1,5 +0,0 @@
-import { Transform } from './types';
-/**
- * Replaces svelte properties with their tailwind compatible syntax.
- */
-export declare const svelte: Transform;
