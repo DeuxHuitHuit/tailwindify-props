fc422e0e40aab7633dd1a780cd52ab14e209ef15
Felix Guerin <guerinfelix08@gmail.com>
2023-10-19 12:04:13 -0400

    git ignore dist (#8)

--- dist/helpers/extractTailwindifiedProps.d.ts
+++
@@ -1,8 +0,0 @@
-import { ExtractedTailwindifiedProps } from '../types';
-/**
- * Looks for calls to tailwindify()
- * and extracts the prop and prefix name.
- * @param {string} content
- * @return Record<string, string>
- */
-export declare const extractTailwindifiedProps: (content: string) => ExtractedTailwindifiedProps;
--- dist/helpers/mergeUnknownPropsIntoReplacers.d.ts
+++
@@ -1,7 +0,0 @@
-import { ExtractedTailwindifiedProps, Replacers } from '../types';
-/**
- * Merges props into the global replacer dictionary.
- * @param {Record<string, string>} props
- * @returns undefined
- */
-export declare const mergeUnknownPropsIntoReplacers: (replacers: Replacers, props: ExtractedTailwindifiedProps) => Replacers;
